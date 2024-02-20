230198bb159129d097880dc621cb172467356cf4
Shanta1 <101503939+dffafaf@users.noreply.github.com>
2024-02-20 23:05:40 +0600

    finish my work

---
+++ script/bus-tickets.js
@@ -0,0 +1,9 @@
+function busTicket(){
+    const bannerSection = document.getElementById('banner-section');
+    bannerSection.classList.add('hidden');
+
+    
+
+    const paribahanSection = document.getElementById('paribahan-section');
+    paribahanSection.classList.remove('hidden');
+}
